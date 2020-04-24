11e865c544278a5e926444cf016f2f6331be5ce2
millerb08 <miller.steffens@gmail.com>
2020-04-24 12:11:43 -0500

    init and example test

---
+++ tests/FirstTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class FirstTest extends PHPUnit\Framework\TestCase{
+    function firstAssertion(){
+        $this->assertTrue(false);
+    }
+
+    function testSecondAssertion(){
+        $this->assertTrue(false);
+    }
+
+    /** @test */
+    function thirdAssertion(){
+        $this->assertTrue(true);
+    }
+}
